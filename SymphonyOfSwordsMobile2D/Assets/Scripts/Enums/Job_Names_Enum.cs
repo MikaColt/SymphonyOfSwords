ea117b089c1d5ff328f81a7f52626e6a7d75ded0
MikaColt <mcrisafi@live.com>
2021-10-28 00:29:20 -0400

    Jobs Names Enum
    Cleaned up the Demo Map a little bit.

---
+++ SymphonyOfSwordsMobile2D/Assets/Scripts/Enums/Job_Names_Enum.cs
@@ -0,0 +1,71 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+enum Job_Names_Enum
+{
+
+    //tier 1
+    Apprentice,
+    Recruit,
+
+    //tier 2
+    //physical
+    Soldier,
+    Scout,
+    Mercenary,
+
+    //magical
+    Healer,
+    Magician,
+    Templar,
+    Hexmage,
+
+
+    //tier 3
+    //physical
+    Knight,
+    Archer,
+    Rogue,
+    Berserker,
+
+    //magical
+    Priest,
+    Arcanist,
+    Warlock,
+    Druid,
+
+
+    //tier 4
+    //Magical
+    Bishop,
+    Sage,
+    Necromancer,
+    Oracle,
+
+    //physical
+    Marauder,
+    Sentinel,
+    Marksman,
+    Thief,
+    Cavalry,
+
+
+
+    //tier 5
+    //Magical
+    Saint,
+    Magi,
+    Lich,
+    Elemental,
+
+    //Physical
+    Reaver,
+    Gaurdian,
+    Lancer,
+    Sniper,
+    Assassin
+
+
+};
+
